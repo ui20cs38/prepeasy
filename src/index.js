eb3fa664fec137ef900a111bfc200db29758d2cd
ui20cs38 <harshmavani06@gmail.com>
2023-06-07 13:55:21 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./componets/Homepage";
+import Login from "./Login";
+import List from "./componets/list/List";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/list" element={<List />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
