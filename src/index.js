e1b690fdb807ef6bb648ece1e9957aeae69126e7
ui20cs38 <harshmavani06@gmail.com>
2023-06-10 10:05:18 +0530

    profile page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./componets/Homepage";
+import Profile from "./componets/Profile";
 import Login from "./Login";
 import List from "./componets/list/List";
+
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/list" element={<List />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+
       </Routes>
     </BrowserRouter>
   );
